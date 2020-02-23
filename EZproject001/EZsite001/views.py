b71daaa73f1134d70e92717d5c6f705360cb9a37
Yizhe Zhang <zhang.yizhe@husky.neu.edu>
2020-02-22 23:51:28 -0500

    update cmd

--- EZproject001/EZsite001/views.py
+++ EZproject001/EZsite001/views.py
@@ -84,9 +84,9 @@ def cmd(request):
     if request.method == "GET":
         return render(request, 'cmd.html')
     elif request.method == "POST":
-        textarea = request.POST.get('textarea')
+        input = request.POST.get('input')
         result_eval = lambda x: eval(x)
-        return render(request, 'cmd.html', {'text': result_eval(textarea)})
+        return render(request, 'cmd.html', {'text': result_eval(input)})
 
 
 def pkb(request):
